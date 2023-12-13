08eecc2167bcdd1d48a2c534ed594127765f8e80
Sehun Chun <schun@palmtree.local>
2023-12-13 16:45:01 +0900

    mpirun

--- solvers/DiffusionSolver/MMFNeuralEP.cpp
+++ solvers/DiffusionSolver/MMFNeuralEP.cpp
@@ -2385,7 +2385,9 @@ void MMFNeuralEP::DoOdeRhsNeuralEP2Dbi(
         Array<OneD, NekDouble> phie = Derivephie(inarray[0]);
         
         // Compute (1/C_n/r) * \nabla^2 \phi_e
-        extcurrent = ComputeMMFDiffusion(m_movingframes, phie);
+        // extcurrent = ComputeMMFDiffusion(m_movingframes, phie);
+
+        extcurrent = ComputeEuclideanDiffusion(phie);
 
         // extra current caused by phi_e only occurs in the intracellular space
         Vmath::Vmul(nq, m_intrazone, 1, extcurrent, 1, extcurrent, 1);
@@ -2427,7 +2429,9 @@ Array<OneD, NekDouble> MMFNeuralEP::Derivephie(
     // // Compute \nabla \sigma_i \nabla phi_m and use it as point sources for
     // phi_e. This is equivalently achieved by removing all the point sources in
     // myelinnated fiber region.
-    Array<OneD, NekDouble> phimLaplacian = ComputeMMFDiffusion(m_helmfmovingframes, phim);
+    // Array<OneD, NekDouble> phimLaplacian = ComputeMMFDiffusion(m_helmfmovingframes, phim);
+
+    Array<OneD, NekDouble> phimLaplacian = ComputeEuclideanDiffusion(phim);
 
     // Only nonzero for node.
     Vmath::Vmul(nq, m_nodezone, 1, phimLaplacian, 1, phimLaplacian, 1);
