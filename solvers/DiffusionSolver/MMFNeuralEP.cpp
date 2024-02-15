feb2c34f695180c4ca57dd3b1596a28f8402cf07
Sehun Chun <schun@palmtree.local>
2024-02-15 11:39:50 +0900

    m_phie

--- solvers/DiffusionSolver/MMFNeuralEP.cpp
+++ solvers/DiffusionSolver/MMFNeuralEP.cpp
@@ -324,9 +324,9 @@ void MMFNeuralEP::v_InitObject(bool DeclareFields)
         case eNeuralEP2Dbi:
         {
             // Expression of phie moving frames in phieMMFdirStr
-            std::string phieMMFdirStr = "LOCAL";
-            m_session->LoadSolverInfo("phieMMFDir", phieMMFdirStr, "LOCAL");
-            SpatialDomains::GeomMMF phieMMFdir = FindMMFdir(phieMMFdirStr);
+            // std::string phieMMFdirStr = "LOCAL";
+            // m_session->LoadSolverInfo("phieMMFDir", phieMMFdirStr, "LOCAL");
+            // SpatialDomains::GeomMMF phieMMFdir = FindMMFdir(phieMMFdirStr);
 
             // Array<OneD, Array<OneD, NekDouble>> m_phieunitMF(m_mfdim);
             // for (int i=0; i<m_mfdim; ++i)
@@ -381,10 +381,10 @@ void MMFNeuralEP::v_InitObject(bool DeclareFields)
             // std::cout << "Set up moving frames with m_phieAniStrength ==================== " << std::endl;
             // SetUpMovingFrames(phieMMFdir, m_phieAniStrength, m_phiemovingframes);
 
-            // std::string phieMMFdirStr = "TangentX";
-            // m_session->LoadSolverInfo("phieMMFDir", phieMMFdirStr, "LOCAL");
+            std::string phieMMFdirStr = "TangentX";
+            m_session->LoadSolverInfo("phieMMFDir", phieMMFdirStr, "LOCAL");
 
-            // SpatialDomains::GeomMMF phieMMFdir = FindMMFdir(phieMMFdirStr);
+            SpatialDomains::GeomMMF phieMMFdir = FindMMFdir(phieMMFdirStr);
             SetUpMovingFrames(phieMMFdir, m_phieAniStrength, m_unitmovingframes);
 
             NekDouble sigma_e_ratio = m_AnisotropyStrength / m_ratio_re_ri;
