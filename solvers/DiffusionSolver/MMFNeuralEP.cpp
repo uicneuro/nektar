72eefcdf750c5ed93facc842094d9f6b5b00376a
Sehun Chun <schun@broom.local>
2024-04-25 19:24:21 +0900

    Rerun

--- solvers/DiffusionSolver/MMFNeuralEP.cpp
+++ solvers/DiffusionSolver/MMFNeuralEP.cpp
@@ -1745,7 +1745,7 @@ void MMFNeuralEP::DoSolveMMFZero()
 
             std::cout << fulltext << "\n" << std::endl;
 
-            // PlotTimeMap(m_ValidTimeMap, m_AniStrength, TimeMap, nchk);
+            PlotTimeMap(m_ValidTimeMap, m_AniStrength, TimeMap, nchk);
             Checkpoint_Output(nchk++);
 
             doCheckTime = false;
