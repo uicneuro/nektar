4759e2d168758af223ac5b9c6bf78ce85324d4bd
Sehun Chun <schun@MacBook-Pro-M4.local>
2025-10-14 10:55:21 +0900

    biMultiv2

--- solvers/MMFSolver/MMFNeuralEP.cpp
+++ solvers/MMFSolver/MMFNeuralEP.cpp
@@ -89,11 +89,7 @@ void MMFNeuralEP::v_InitObject(bool DeclareFields)
         // NeuralEP2DbiMulti model for two fibers with a signel phie
         case 3:
         {
-            if(m_NeuralEPType == eNeuralEP2DbiMulti)
-            {
-                m_phimvar = 1;
-            }
-            else if(m_NeuralEPType == eNeuralEP2DbiMultiv2)
+            if( (m_NeuralEPType == eNeuralEP2DbiMulti) || (m_NeuralEPType == eNeuralEP2DbiMultiv2) )
             {
                 m_phimvar = 1;
             }
@@ -3961,11 +3957,9 @@ void MMFNeuralEP::DoOdeRhsNeuralEP2DbiMultiv2(
             Vmath::Zero(nq, outarray[i], 1);
         }
     }
-    std::cout << "ODE HERE 1 " << std::endl;
+
     // 1. Reaction Term (FHN or H-H ion current model)
     m_neuron->TimeIntegrateMulti(numfiber, m_zoneindexfiber, inarray, outarray, time, Temp);
-
-    std::cout << "ODE HERE 2 " << std::endl;
 
     Array<OneD, NekDouble> tmp(nq);
     Array<OneD, NekDouble> phie(nq, 0.0);
@@ -3975,13 +3969,9 @@ void MMFNeuralEP::DoOdeRhsNeuralEP2DbiMultiv2(
         m_stimulus[n]->Update(m_zoneindexfiber[n], outarray[n], time);
     }
 
-    std::cout << "ODE HERE 3 " << std::endl;
-
-
     Array<OneD, NekDouble> phim(nq);
     Vmath::Vadd(nq, inarray[0], 1, inarray[1], 1, phim, 1);
     m_fields[phievar]->UpdatePhys() = ComputeFieldPhiefiberv2(phievar, phim);
-    std::cout << "ODE HERE 4 " << std::endl;
 
     // 4. Compute \nabla \cdot (\sigma_i \nabla \phi_e) and add to membrane current
     Array<OneD, NekDouble> phiecurrent(nq);
@@ -3998,7 +3988,6 @@ void MMFNeuralEP::DoOdeRhsNeuralEP2DbiMultiv2(
             }
         }
     }
-    std::cout << "ODE HERE 5 " << std::endl;
 
     if (m_explicitDiffusion)
     {
@@ -4416,6 +4405,7 @@ void MMFNeuralEP::v_SetInitialConditions(NekDouble initialtime,
         }
 
         case eNeuralEP2DbiMulti:
+        case eNeuralEP2DbiMultiv2:
         {
             int numfiber = m_numfiber;
 
